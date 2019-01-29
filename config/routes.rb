060332c1063cab450023320c6b4150a4d65a26a0
dossy007 <toshi@toshinoMacBook-ea.local>
2019-01-29 21:23:40 +0900

    Change root
    rootを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root "choco#index"
-  root 'choco#new'
+  root 'choco#index'
   resources :choco
     # get "choco/:id/create" =>"choco#create"
 # ネストすれば行けるかも
