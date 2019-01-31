019b1b29fb1d58b9df31a66668427443a16c7f56
dossy007 <toshi@toshinoMacBook-ea.local>
2019-01-31 17:37:34 +0900

    Add image
    imageを追記

--- app/controllers/choco_controller.rb
+++ app/controllers/choco_controller.rb
@@ -8,7 +8,7 @@ class ChocoController < ApplicationController
 
 	def create
 		@choco = Chocolate.create(:text => create_params[:text], :content => create_content,:name => params[:name],:image => "http://psnews.jp/cat/uploads/2017/02/cat-1646566_1280.jpg")
-		@post_id = @choco.id
+		@choco.image = "あか時計.jpg"
 		redirect_to choco_path(@choco.id)
 	end
 
@@ -26,10 +26,5 @@ class ChocoController < ApplicationController
 		# params[:text] || ""
 	end
 
-	def create_content #valueを変換する処理  "うんこ" =>"ダイヤモンド"
-		# 上段は汚いワード
-		create_values.gsub(/ruby|クソ|うんこ|しね|死ね|あほ|/,"ruby" => "python", "クソ" => "優しい","うんこ"=>"ダイヤモンド","しね" =>"好きすぎてたまりません","死ね" =>"好きすぎてたまりません",
-			"あほ"=>"天才").##以下は罵りワード
-		gsub(/aaa/,"aaa"=>"bbb")
-	end
+
 end
