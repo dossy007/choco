c442c48dd59cf9be6363d1e2b5ac6809e66fd7ee
dossy007 <toshi@toshinoMacBook-ea.local>
2019-01-31 17:37:00 +0900

    Add application controller
    create_content methodをapplication controllerに追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	def create_content #valueを変換する処理  "うんこ" =>"ダイヤモンド"
+		# 上段は汚いワード
+		create_values.gsub(/ruby|クソ|うんこ|しね|死ね|あほ|/,"ruby" => "python", "クソ" => "優しい","うんこ"=>"ダイヤモンド","しね" =>"好きすぎてたまりません","死ね" =>"好きすぎてたまりません",
+			"あほ"=>"天才").##以下は罵りワード
+		gsub(/aaa/,"aaa"=>"bbb")
+	end
 end
